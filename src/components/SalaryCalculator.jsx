95a29cccf816664b91f8e7cc9c308b606fd1f49b
bojan.savic <bojan@test.com>
2023-02-08 13:36:33 +0100

    Project Update

--- src/components/SalaryCalculator.jsx
+++ src/components/SalaryCalculator.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function SalaryCalculator() {
   let [bruto, setBruto] = useState(1016.39);
   let [dobitak, setDobitak] = useState(1000.0);
-  let [umanjenje, setUmanjenje] = useState(0.00);
+  let [umanjenje, setUmanjenje] = useState(0.0);
   let [neto, setNeto] = useState(700.0);
-  let [isplata, setIsplata] = useState(0.00);
+  let [isplata, setIsplata] = useState(0.0);
   let porez = 0;
   let solidarnost = 1.75;
 
@@ -24,11 +24,11 @@ function SalaryCalculator() {
   dobitak = Math.min(bruto, dobitak);
 
   //Umanjenje po poreskoj kartici
-  if ((dobitak + umanjenje) > bruto) {
+  if (dobitak + umanjenje > bruto) {
     umanjenje = bruto - dobitak;
     umanjenje = umanjenje.toFixed(2);
   }
-    
+
   //Porez batice...
   let calculatePorez = () => {
     porez = (bruto - (dobitak + umanjenje) * (8 / 100)).toFixed(2);
@@ -39,8 +39,8 @@ function SalaryCalculator() {
   neto = neto.toFixed(2);
 
   //Doprinosi za solidarnost
-  solidarnost = neto * (0.25 / 100)
-  solidarnost = solidarnost.toFixed(2)
+  solidarnost = neto * (0.25 / 100);
+  solidarnost = solidarnost.toFixed(2);
 
   //Isplata radniku
   isplata = (neto - solidarnost).toFixed(2);
@@ -48,160 +48,223 @@ function SalaryCalculator() {
   //Ukupne obaveze
   let obaveze = doprinosi + porez + solidarnost;
 
-  const calculateSalary = () => {
-    solidarnost = neto * (0.25 / 100);
-  };
-
   return (
-    <div className="divTable">
-      <div className="divTableBody">
-        <div className="divTableRow">
-          <div className="divTableCell">Bruto plata</div>
-          <div className="divTableCell">
-            B ={" "}
-            <input
-              style={{ background: "lightblue" }}
-              value={bruto}
-              type="text"
-              onChange={(event) => {
-                setBruto(event.target.value);
-              }}
-            />{" "}
-            KM
-          </div>
-          <div className="divTableCell">
-            {`B=N+D+P, ako je N<=(O+U)*0.69, onda B=N/0.69, ako je N>(O+U)*0.69, onda B=(N-(O+U)*0.08)/0.61`}
-          </div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Ukupni doprinosi</div>
-          <div className="divTableCell">
-            D ={" "}
-            <input
-              style={{ background: "#f75640" }}
-              value={doprinosi}
-              type="text"
-            />{" "}
-            KM
-          </div>
-          <div className="divTableCell">D=B*31%</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Doprinosi za PIO</div>
-          <div className="divTableCell">
-            PIO = <input readOnly value={pio} type="text" /> KM
-          </div>
-          <div className="divTableCell">PIO-18.5%</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Doprinosi za ZDR</div>
-          <div className="divTableCell">
-            ZDR = <input readOnly value={zdr} type="text" /> KM
-          </div>
-          <div className="divTableCell">ZDR-10.2%</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Doprinosi za DJ</div>
-          <div className="divTableCell">
-            DJ = <input readOnly value={djDoprinosi} type="text" /> KM
-          </div>
-          <div className="divTableCell">DJ-1.70%</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Doprinosi za NZ</div>
-          <div className="divTableCell">
-            NZ = <input readOnly value={nzDprinosi} type="text" /> KM
-          </div>
-          <div className="divTableCell">NZ-0.6%</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Lični odbitak</div>
-          <div className="divTableCell">
-            O ={" "}
-            <input
-              value={dobitak}
-              type="text"
-              onChange={(event) => {
-                setDobitak(event.target.value);
-              }}
-            />{" "}
-            KM
-          </div>
-          <div className="divTableCell">{`ako je O>B onda je O=B`}</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Umanjenje po poreskoj kartici</div>
-          <div className="divTableCell">
-            U ={" "}
-            <input
-              value={umanjenje}
-              type="text"
-              onChange={(event) => {
-                setUmanjenje(event.target.value);
-              }}
-            />{" "}
-            KM
-          </div>
-          <div className="divTableCell">{`ako je O+U>B onda je U=B-O`}</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Porez</div>
-          <div className="divTableCell">
-            P = <input readOnly value={porez} type="text" /> KM
-          </div>
-          <div className="divTableCell">{`P=(B-(O+U))*8%`}</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Neto plata:</div>
-          <div className="divTableCell">
-            N ={" "}
-            <input
-              value={neto}
-              type="text"
-              onChange={(event) => {
-                setNeto(event.target.value);
-              }}
-            />{" "}
-            KM
-          </div>
-          <div className="divTableCell">N=B-D-P</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Doprinos za solidarnost:</div>
-          <div className="divTableCell">
-            S = <input readOnly value={solidarnost} type="text" /> KM
-          </div>
-          <div className="divTableCell">S=N*0.25%</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell">Isplata radniku:</div>
-          <div className="divTableCell">
-            I ={" "}
-            <input
-              value={isplata}
-              type="text"
-              onChange={(event) => {
-                setIsplata(event.target.value);
-              }}
-            />{" "}
-            KM
-          </div>
-          <div className="divTableCell">I=N-S</div>
-        </div>
-        <div className="divTableRow">
-          <div className="divTableCell"></div>
-          <div className="divTableCell"></div>
-          <div className="divTableCell">
-            Ukupne obaveze: &nbsp;
-            <input value={obaveze} type="text" />
-          </div>
-        </div>
+    <div className="max-w-7xl">
+      <div className="relative overflow-x-auto rounded">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"></thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Bruto plata
+              </th>
+              <td className="px-6 py-4">
+                {" "}
+                B ={" "}
+                <input
+                  style={{ background: "lightblue" }}
+                  value={bruto}
+                  type="text"
+                  onChange={(event) => {
+                    setBruto(event.target.value);
+                  }}
+                />{" "}
+                KM
+              </td>
+              <td className="px-6 py-4">
+                {" "}
+                {`B=N+D+P, ako je N<=(O+U)*0.69, onda B=N/0.69, ako je N>(O+U)*0.69, onda B=(N-(O+U)*0.08)/0.61`}
+              </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Ukupni doprinosi
+              </th>
+              <td className="px-6 py-4">
+                {" "}
+                D ={" "}
+                <input
+                  style={{ background: "#f75640" }}
+                  value={doprinosi}
+                  type="text"
+                />{" "}
+                KM
+              </td>
+              <td className="px-6 py-4">D=B*31%</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Doprinosi za PIO
+              </th>
+              <td className="px-6 py-4">
+                PIO = <input readOnly value={pio} type="text" /> KM
+              </td>
+              <td className="px-6 py-4">PIO-18.5%</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Doprinosi za ZDR
+              </th>
+              <td className="px-6 py-4">
+                ZDR = <input readOnly value={zdr} type="text" /> KM
+              </td>
+              <td className="px-6 py-4">ZDR-10.2%</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Doprinosi za DJ
+              </th>
+              <td className="px-6 py-4">
+                DJ = <input readOnly value={djDoprinosi} type="text" /> KM
+              </td>
+              <td className="px-6 py-4">DJ-1.70%</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Doprinosi za NZ
+              </th>
+              <td className="px-6 py-4">
+                NZ = <input readOnly value={nzDprinosi} type="text" /> KM
+              </td>
+              <td className="px-6 py-4">NZ-0.6%</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Lični odbitak
+              </th>
+              <td className="px-6 py-4">
+                O ={" "}
+                <input
+                  value={dobitak}
+                  type="text"
+                  onChange={(event) => {
+                    setDobitak(event.target.value);
+                  }}
+                />{" "}
+                KM
+              </td>
+              <td className="px-6 py-4">{`ako je O>B onda je O=B`}</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Umanjenje po poreskoj kartici
+              </th>
+              <td className="px-6 py-4">
+                U ={" "}
+                <input
+                  value={umanjenje}
+                  type="text"
+                  onChange={(event) => {
+                    setUmanjenje(event.target.value);
+                  }}
+                />{" "}
+                KM
+              </td>
+              <td className="px-6 py-4">{`ako je O+U>B onda je U=B-O`}</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Porez
+              </th>
+              <td className="px-6 py-4">
+                P = <input readOnly value={porez} type="text" /> KM
+              </td>
+              <td className="px-6 py-4">{`P=(B-(O+U))*8%`}</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Neto plata:
+              </th>
+              <td className="px-6 py-4">
+                N ={" "}
+                <input
+                  value={neto}
+                  type="text"
+                  onChange={(event) => {
+                    setNeto(event.target.value);
+                  }}
+                />{" "}
+                KM
+              </td>
+              <td className="px-6 py-4">N=B-D-P</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Doprinos za solidarnost:
+              </th>
+              <td className="px-6 py-4">
+                S = <input readOnly value={solidarnost} type="text" /> KM
+              </td>
+              <td className="px-6 py-4">S=N*0.25%</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Isplata radniku:
+              </th>
+              <td className="px-6 py-4">
+                I ={" "}
+                <input
+                  value={isplata}
+                  type="text"
+                  onChange={(event) => {
+                    setIsplata(event.target.value);
+                  }}
+                />{" "}
+                KM
+              </td>
+              <td className="px-6 py-4">I=N-S</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white"
+              ></th>
+              <td className="px-6 py-4"></td>
+              <td className="px-6 py-4">
+                <label>Ukupne obaveze: </label>
+                <input value={obaveze} type="text" /> KM
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <button type="button" onClick={() => calculateSalary()}>
-        Izracunaj
-      </button>
     </div>
-    
   );
 }
 
